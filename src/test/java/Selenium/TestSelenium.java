977eef0ef12a171dea56ea081de0e2a833a6e5b0
Edgar-Ovando <edgaar.miguell@gmail.com>
2026-01-16 14:08:22 -0600

    Test Pipeline Selenium Java Jenkins

---
+++ src/test/java/Selenium/TestSelenium.java
@@ -0,0 +1,93 @@
+package Selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class TestSelenium {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void loadDriver(){
+        //Configurar la ruta del chromedriver
+        System.setProperty("webdriver.chrome.driver","/Users/edgar-ovando/Downloads/chromedriver-mac-arm64/chromedriver");
+        //Crear una instancia
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testBrowser(){
+        driver.get("https://www.google.com/");
+        String titulo = driver.getTitle();
+        Assert.assertEquals("Google",titulo);
+    }
+
+   @Test
+    public void ingresarTexto(){
+        driver.get("https://www.google.com/");
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("Historia de Linux");
+        searchbox.submit();
+    }
+
+
+    // -- Localizadores Xpath
+    @Test
+    public void testXPath(){
+        driver.get("https://www.google.com/");
+        WebElement searchbox = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+        searchbox.sendKeys("Historia de Selenium");
+        searchbox.submit();
+        //driver.findElement(By.xpath("//*[@id=\"_hrrWaLGbFKzf2roPzuOwkQw_32\"]/div[1]/div/div/div[1]/div[3]/a/div")).click();
+    }
+
+    //Manejo de Excepciones
+    @Test
+    public void testExceptions(){
+        driver.get("https://www.google.com/");
+        try {
+            driver.findElement(By.id("no Existe Id")).click();
+        }catch (NoSuchElementException e){
+            System.out.println("Error - El elemento no fue encontrado " + e.getMessage());
+        }catch (TimeoutException e){
+            System.out.println("Error - Tiempo de espera agotado " +e.getMessage());
+        }finally {
+            //cerrar el navegador
+            //driver.quit();
+            System.out.println("Se completo el manejo de Exceptions");
+        }
+    }
+
+    //Cerrar Navegador
+    @AfterTest
+    public void closeBrowser(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+    //API Testing Pokemon
+    /*
+    @Test
+    public void testGetUser(){
+        baseURI = "https://pokeapi.co/api/v2/pokemon/";
+
+        String body = given()
+                .when()
+                .get("35")
+                .then()
+                .statusCode(200)
+
+                .extract().body().asString();
+        System.out.println(body);
+    }*/
+
+
+}
---
+++ src/test/java/Selenium/TestSeleniumII.java
@@ -0,0 +1,129 @@
+package Selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestSeleniumII {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void loadDriver(){
+        //Configurar Web driver Manager
+        WebDriverManager.chromedriver().setup();
+
+        //Configurar la ruta del chromedriver
+        //System.setProperty("webdriver.chrome.driver","/Users/edgar-ovando/Downloads/chromedriver-mac-arm64/chromedriver");
+
+        //Crear una instancia
+        driver = new ChromeDriver();
+
+    }
+
+    // -- Window ALERT
+    @Test
+    public void windowAlert(){
+
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        driver.get("https://onecompiler.com/html/3xfmfsrwk");
+
+        // Encontrar el button y darle click
+        WebElement runbtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='MuiGrid-root MuiGrid-direction-xs-row mui-1n5khr6']/div//button[text()='Run ']")));
+        runbtn.click();
+
+        //Alerta appear
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        System.out.println("Contenido de la alerta: "+alert.getText());
+        alert.accept();
+
+    }
+
+    // Window PESTAÑAS
+    @Test
+    public void windowPestaña(){
+        driver.get("https://www.facebook.com/");
+        String original = driver.getWindowHandle();
+
+        //Abrir nueva pestaña
+        WebDriver newtab = driver.switchTo().newWindow(WindowType.TAB);
+        newtab.get("https://www.facebook.com/help/568137493302217");
+        String otratab = newtab.getWindowHandle();
+
+
+        System.out.println(original);
+        System.out.println(otratab);
+        driver.switchTo().window(original);
+    }
+
+    //Window iFrame Pagina web dentro de otra pagina web
+    @Test
+    public void testIFrame(){
+        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe");
+
+        driver.switchTo().frame("iframeResult");
+        driver.switchTo().frame(driver.findElement(By.xpath("/html/body/iframe")));
+
+
+        WebElement iframeElement = driver.findElement(By.xpath("//*[@id=\"tnb-login-btn\"]"));
+        iframeElement.click();
+
+    }
+
+    //Espera Explicita
+    @Test
+    public void testExplicito(){
+        WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.facebook.com/");
+
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        emailField.sendKeys("correoejemplo@gmail.com");
+
+    }
+
+    //Espera Implicita
+    @Test
+    public void testImplicito(){
+        driver.get("https://www.facebook.com/");
+
+        //Implicit Wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebElement login = wait.until(ExpectedConditions.elementToBeClickable(By.name("login")));
+    }
+
+    //Assert TestNG
+    @Test
+    public void testAssertion(){
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+        String expectedTitle = "Google";
+        String actualTitle = driver.getTitle();
+
+        //Assert
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+    }
+
+    //Cerrar Navegador
+    @AfterTest
+    public void closeBrowser(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
